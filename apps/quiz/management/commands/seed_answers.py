2e184087b094902f5b418874d7cd8211b5ce8f31
dunt <ntdu198@gmail.com>
2024-12-27 09:13:32 +0700

    chore: seed answer

--- apps/quiz/management/commands/seed_answers.py
+++ apps/quiz/management/commands/seed_answers.py
@@ -9,35 +9,13 @@ class Command(BaseCommand):
     help = 'Seed the database with comments'
 
     def handle(self, *args, **kwargs):
-        test_data = {
-            "Addresses": [
-                {
-                "PSId": "00279277",
-                "Email": "dunt14@fpt.com"
-                },
-                {
-                "PSId": "00183946",
-                "Email": "philk@fpt.com"
-                }
-            ],
-            "MessageContent": {
-                "modelCategory": "40",
-                "sound": "sound_noti.mp3",
-                "title": "Your Title",
-                "message": "Short description or full message here",
-                "priority": 5,
-                "model": {
-                    "LogId": "674d15b804237f2bd4df7d2c",
-                    "Data": {
-                        "Key1": "Value1",
-                        "Key2": "Value2"
-                    }
-                }
-            }
-        }
-
         producer = KafkaProducer()
-        producer.produce(KafkaTopic.QUIZ_ANSWER.value, key='0', value=json.dumps(test_data))
-        producer.flush()
+
+        for i in range(1, 60):
+            test_data = {
+                "id": i,
+            }
+            producer.produce(KafkaTopic.QUIZ_ANSWER.value, key='0', value=json.dumps(test_data))
+            producer.flush()
 
         self.stdout.write(self.style.SUCCESS(F'Successfully seeded comments into the database'))
